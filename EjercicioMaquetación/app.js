405e31a3c1256a7c9587e3cb14d5ec6ec4aba5c1
Aylen <lag9512mail.com>
2023-07-22 14:04:34 -0300

    Cambio de nombre de styles.css a mediaMobile.css. Se agregó funcionalidad para alternar bloques haciendo click en los títulos

--- EjercicioMaquetación/app.js
+++ EjercicioMaquetación/app.js
@@ -4,27 +4,32 @@ const specs = document.querySelector('.menu__h2--specs')
 const boxSection = document.querySelector('.box')
 const specsSection = document.querySelector('.specs')
 
-boxSection.style.display = 'flex';
-specsSection.style.display = 'none'
-
 box.addEventListener('click', () => {
- if(boxSection.style.display === 'flex') { 
-  return
- }
- if(boxSection.style.display === 'none') { 
-  boxSection.style.display = 'flex'
-  return
- }
+  if(!boxSection.classList.contains('visible')){
+    boxSection.classList.add('visible')
+  }
+
+  if(boxSection.classList.contains('invisible')){
+    boxSection.classList.replace('invisible', 'visible')
+  }
+  specsSection.classList.add('invisible')
+  specsSection.classList.remove('visible')
 })
  
 
 specs.addEventListener('click', () => {
- if(specsSection.style.display === 'flex') {
-  return
- }
- if(specsSection.style.display === 'none') { 
-  specsSection.style.display = 'flex'
-  return
- }
+  console.log(specsSection.classList)
+  if(!specsSection.classList.contains('visible')){
+    specsSection.classList.add('visible')
+  }
+  if(specsSection.classList.contains('invisible')){
+    specsSection.classList.replace('invisible', 'visible')
+  }
+  boxSection.classList.add('invisible')
+  boxSection.classList.remove('visible')
 })
 
+//Si tiene la clase "visible", mantenerla.
+//Si tiene la clase invisible, reemplazarla por visible
+//sacarle la clase "visible" al otro contenedor
+//ponerle la clase invisible al otro contenedor
